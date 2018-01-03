712fcfc7db05d4a223d0432adf40b91f87f4de70
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-03 01:15:34 -0500

    made initial test for to_hash method

--- 03/test_team.rb
+++ 03/test_team.rb
@@ -4,4 +4,15 @@ require './team.rb'
 
 class TestTeam < MiniTest::Test
 
+  def setup
+    @team = Team.new('patriots', 'nfl', ['Tom Brady', 'Rob Gronkowski'])
+  end
+
+  def test_to_hash
+    hash = @team.to_hash
+    assert_equal(hash[:team_name], 'patriots')
+    assert_equal(hash[:level], 'nfl')
+    assert_equal(hash[:points], 0)
+  end
+
 end
